0c186ee04775e29bda4f88e4acd4581c1e3ace8b
AliHussainKhamis <alihussainkh3@gmail.com>
2025-07-09 13:57:03 +0300

    Handle click and increment score

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,61 @@
-
 function init(){
-
-  function createGrid(){
-    // for every cell create a div
-    // append this cell to our grid
+    // All code goes here
     const gridElem = document.querySelector('.grid')
+    const scoreElem = document.querySelector('#score-display')
 
     const cells = []
     const gridWidth = 10
-    const numberOfCells = gridWidth*gridWidth
+    const numberOfCells = gridWidth * gridWidth
+
+    let duckPosition = 3
+    let score = 0
 
 
-    for(let i =0; i<numberOfCells; i++){
-
-        const cell = document.createElement('div')
-        cell.classList.add('duck') // **** ADDS DUCKS TO THE CLASS LIST (cell(div))
-        cell.textContent = i
-        cells.push(cell)
-
-        gridElem.appendChild(cell)
+// 2
+    function addDuck(){
+        cells[duckPosition].classList.add('duck')
     }
-    console.log(cells)
-  }
+// 3
+    function removeDuck(){
+        cells[duckPosition].classList.remove('duck')
+    }
+// 4
+    function play(){
+        setInterval(() => {
+            removeDuck()
+            duckPosition = Math.floor(Math.random() * numberOfCells)
+            addDuck()
+        }, 3000)
+    }
+// 5 add event listener to the cells & Handle click to add  to score
+    
+    function handleClick(event){
+        console.log('Handle Click has run!')
+        // handleClick(event)
+        if(event.target.classList.contains('duck')){
+            score += 10
+            scoreElem.textContent = `Your score is ${score}`
+            console.log(score)
+        }
+    }
+// 1
+    function createGrid(){
+        // for every cell that we require create a div
+        // append this cell to our grid
+        for (let i = 0; i < numberOfCells; i++){
+            const cell = document.createElement('div')
+            // cell.classList.add('duck')
+            cell.textContent = i
+            cells.push(cell)
+            cell.addEventListener('click', handleClick)
+            gridElem.appendChild(cell)
+        }
+        console.log(cells)
+    }
+    // result
+    createGrid()
+    play()
 
-  createGrid()
 }
 
-document.addEventListener('DOMContentLoaded',init);
+document.addEventListener('DOMContentLoaded', init)
